561cf072f2f5b8080fdcd76b9a69d16109cab575
Gary Bishop <gary.bishop.unc@gmail.com>
2011-01-09 19:39:38 -0500

    permalink

--- _config.py
+++ _config.py
@@ -55,7 +55,7 @@ blog.timezone = "US/Eastern"
 blog.post_excerpts.enabled = True
 blog.post_excerpts.word_length = 25
 blog.posts_per_page = 10
-blog.auto_permalink.path = site.root + blog.path + "/:year/:month/:day/:title"
+blog.auto_permalink.path = "/" + blog.path + "/:year/:month/:day/:title"
 
 ### colors
 class Bag(object):
